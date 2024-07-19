cb28dd121a2edd91a683924ca137ba732cfa2938
Lucas Carvalho Malagutti <lucas.malaiorgutti@gmail.com>
2024-07-19 17:01:38 -0300

    Adicionar arquivos de projeto.

---
+++ EnviarMultiplosIDs/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using EnviarMultiplosIDs.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EnviarMultiplosIDs.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public JsonResult SeuMetodo([FromBody] int[] ids)
+        {
+            if (ids == null || ids.Length == 0)
+            {
+                return Json(new { success = false, message = "Nenhum ID foi recebido." });
+            }
+
+            // Processar os IDs
+            foreach (var id in ids)
+            {
+                System.Diagnostics.Debug.WriteLine("ID recebido: " + id);
+            }
+
+            return Json(new { success = true, receivedIds = ids });
+        }
+
+    }
+}
---
+++ EnviarMultiplosIDs/wwwroot/js/site.js
@@ -0,0 +1,30 @@
+﻿document.querySelectorAll('.IdServico').forEach(check => {
+    check.addEventListener('change', () => {
+        const checkboxes = document.querySelectorAll('input[name="idServico"]:checked');
+        let selectedIds = [];
+        checkboxes.forEach((checkbox) => {
+            selectedIds.push(checkbox.value); // Use checkbox.value para coletar os valores
+        });
+        console.log('IDs selecionados dentro do event listener: ', selectedIds);
+        $('#idServicosSelecionados').val(selectedIds.join(','));
+    });
+});
+
+$('#submitBtn').click(function () {
+    const selectedIds = $('#idServicosSelecionados').val().split(',').map(Number);
+    console.log('IDs selecionados no submit: ', selectedIds);
+
+    $.ajax({
+        url: '/Home/SeuMetodo', // Substitua com a URL do seu método no controlador
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(selectedIds),
+        success: function (response) {
+            console.log('IDs recebidos: ', response.receivedIds);
+            alert('Dados enviados com sucesso!');
+        },
+        error: function (xhr, status, error) {
+            alert('Ocorreu um erro: ' + error);
+        }
+    });
+});
